7a06d5719e30f047fd757ab24b1ce231b040f0dc
xavier wu <wuzw3@mail2.sysu.edu.cn>
2015-06-17 15:01:21 +0800

    about flushing the output buffer.

--- IOLibrary/main.cpp
+++ IOLibrary/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
 int main (int argc, char *argv[])
 {
+	cout << "hi!" << endl; // writes hi and a newline, then flushes the buffer
+	cout << "hi!" << flush; // writes hi, then flushes the buffer; adds no data
+	cout << "hi!" << ends; // writes hi and a null, then flushes the buffer
 	return 0;
 }
